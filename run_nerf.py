afc49a3aea23ed7b041705d9f94e9fc90248a9e0
Jxu-Thu <jxu3425@gmail.com>
2023-02-06 15:27:49 +0800

    g

--- run_nerf.py
+++ run_nerf.py
@@ -675,6 +675,8 @@ def train():
     # Prepare raybatch tensor if batching random rays
     N_rand = args.N_rand
     use_batching = not args.no_batching
+    import pdb
+    pdb.set_trace()
     if use_batching:
         # For random ray batching
         print('get rays')
